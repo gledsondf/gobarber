856a9b910b674b7e8c8450032857c3812ff5789f
gledsondf <gledsondf@gmail.com>
2020-02-03 17:59:34 -0300

    modelo
    user modelo e conexão banco

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection;
+  }
+}
