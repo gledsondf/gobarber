af07a304fcaad9d560dc25a9e54b9071bef32c5f
gledsondf <gledsondf@gmail.com>
2020-02-04 23:43:34 -0300

    gerando hash
    gerando hash de senha

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import User from './app/models/User';
 
+import UserController from './app/controllers/UserController';
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name:'Gledson',
-    email:'gledsondf@gmail.com',
-    password_hash:'123456'
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-}); 
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name:'Gledson',
+//     email:'gledsond5f@gmail.com',
+//     password_hash:'123456'
+//   });
+
+//   return res.json(user);
+// }); 
 
 export default routes;
